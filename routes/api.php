34be620e508b419d933d33871c511d7041a7a791
Lenka2023 <lenka20231@gmail.com>
2020-07-09 20:02:59 +0300

    make routes

--- routes/api.php
+++ routes/api.php
@@ -29,11 +29,8 @@ Route::post('/v1', function () {
 /*Route::get('/v1/users/{id?}/{code?}', function ($id=null, $code=null) {
     return 'users='.$id.'code='.$code;
 })->where(['id'=>'^.{36}$', 'code'=>'[0-9]+']);*/
-Route::prefix('/v1/products')->middleware('throttle:2|60,1')->group(function() {
-    Route::get('/{id?}/{code?}', function ($id = null, $code = null) {
-        return 'GROUP users=' . $id . 'code=' . $code;
-    })->where(['id' => '^.{36}$', 'code' => '[0-9]+']);
-    Route::post('/{id}/', function ($id = null) {
-        return 'GROUP post users=' . $id;
-    })->where(['id' => '^.{36}$']);
+Route::prefix('/v1/products')->group(function() {
+    Route::get('/', 'Api\ProductsResource@index');
+    Route::get('/{id?}/{code?}', 'Api\ProductsResource@show');
+
 });
